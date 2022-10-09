5b85299915e3c164fbabc16dce1fb99109ec9d09
Lucas Cruz <lucas.apcruz@gmail.com>
2022-10-09 14:08:48 -0300

    Add basic structure

---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import GlobalStyle from "../globalstyles";
+import { useState } from "react";
+
+export default function App() {
+    return(
+        <>
+            <GlobalStyle/>
+        </>
+    )
+}
