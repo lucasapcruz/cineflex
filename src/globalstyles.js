5de74a8afe4a9cfb99ca56af61463c8a4ff63389
Lucas Cruz <lucas.apcruz@gmail.com>
2022-10-09 18:23:52 -0300

    style: import Roboto font-family

--- src/globalstyles.js
+++ src/globalstyles.js
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from 'styled-components'
  
 const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
 
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
